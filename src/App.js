6e4e664eaa6a7bd813d3b14e519b1d27911334d9
ValeriaLD <leascencovaleria.vl@gmail.com>
2021-03-16 08:21:20 +0200

    tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from "react";
+import AddTask from './components/AddTask';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'TW Attestation',
+        day: 'March 16th at 8:00 AM',
+        reminder: true,
+    }, 
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'March 17th at 2:00 PM',
+        reminder: true,
+    },
+])
+
+const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([ ...tasks, newTask])
 }
+
+
+const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+}
+
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) => 
+    task.id === id ? { ...task, reminder: !task.reminder} : task)
+    )
+}
+
+return (
+  <div className="container">
+    <Header />
+    <AddTask onAdd={addTask} />
+    {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> ) : ('No task to show')}
+  </div>
+);
+
+}
+
 
 export default App;
